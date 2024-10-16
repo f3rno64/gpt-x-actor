e0d27a5663d7b65eee4133b59641195e8d8bcb12
Cris Mihalache <c.mihalache@sportradar.com>
2024-10-16 21:54:38 +0200

    chore: update.

--- scripts/worker.sh
+++ scripts/worker.sh
@@ -3,9 +3,9 @@
 DIR=$(dirname 0)/..
 LOG_DIR="$DIR/logs"
 
-if [ -z "$WORKER_NAME" ]; then
-    echo "Error: WORKER_NAME is not set"
+if [ -z "$WORKER_CONFIG_PATH" ]; then
+    echo "Error: WORKER_CONFIG_PATH is not set"
     exit 1
 fi
 
-export $(cat "$DIR/.env" | xargs) && $(which node) "$DIR/dist/main/$WORKER_NAME.js"
+export $(cat "$DIR/.env" | xargs) && $(which node) "$DIR/scripts/worker.sh"
