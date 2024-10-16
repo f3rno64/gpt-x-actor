0f8a6e80462d1c1b16d652ca36e97f1ebd03b3c1
Cris Mihalache <c.mihalache@sportradar.com>
2024-10-16 23:53:50 +0200

    chore: update.

--- src/main/tweet_reply_worker.ts
+++ src/main/tweet_reply_worker.ts
@@ -2,8 +2,8 @@ import dotenv from 'dotenv'
 import { promises as fs } from 'fs'
 import _isEmpty from 'lodash/isEmpty'
 import { Signale } from 'signale'
-import { callOpenAiApi, loadTemplate, loadDB, promptUser, saveDB } from '../lib'
-import Twitter from 'twitter-api-v2'
+import { promptUser } from '../lib'
+import Twitter, { ETwitterStreamEvent } from 'twitter-api-v2'
 
 dotenv.config()
 
@@ -18,19 +18,11 @@ const getEnvVar = async (key: string, promptMessage: string): Promise<string> =>
 const l = new Signale({ scope: 'index' })
 
 const run = async () => {
+    l.star('Subscribing to live tweet firehose...')
+
     const WORKER_CONFIG_PATH = await getEnvVar('WORKER_CONFIG_PATH', 'Enter the worker config file path:')
 
     l.star(`Started at ${new Date().toISOString()}: ${WORKER_CONFIG_PATH}`)
-
-    const db = await loadDB()
-    const { lastTweetedAtMs = 0 } = db
-    const MIN_TWEET_INTERVAL_MS = await getEnvVar('MIN_TWEET_INTERVAL_MS', 'Enter the minimum tweet interval in milliseconds:')
-    const currentTweetInterval = Date.now() - lastTweetedAtMs
-
-    if (currentTweetInterval <= +MIN_TWEET_INTERVAL_MS) {
-        l.star(`Minimum tweet interval not reached: ${currentTweetInterval}ms elapsed since ${new Date(lastTweetedAtMs).toISOString()}`)
-        return
-    }
 
     let workerConfigJSON: string
     try {
@@ -42,9 +34,6 @@ const run = async () => {
     const workerConfig = JSON.parse(workerConfigJSON)
 
     const {
-        INSTRUCTIONS,
-        ACCOUNT_INFORMATION,
-        HASHTAG_SUGGESTIONS,
         TWITTER_API_KEY,
         TWITTER_API_SECRET,
         TWITTER_ACCESS_TOKEN,
@@ -74,26 +63,47 @@ const run = async () => {
         accessSecret: TWITTER_ACCESS_TOKEN_SECRET
     })
 
-    const tweetTemplate = loadTemplate('tweet.hbs')
-    const tweetGeneratorTemplate = loadTemplate('tweet_generator.prompt.hbs')
-    const tweetHashtagsGeneratorTemplate = loadTemplate('tweet_hashtags_generator.prompt.hbs')
+    l.star('fetching existing stream rules...')
 
-    l.star('Generating tweet...')
+    const rules = await twitterClient.v2.streamRules();
 
-    const tweetContent = await callOpenAiApi(tweetGeneratorTemplate({ accountInformation: ACCOUNT_INFORMATION, instructions: INSTRUCTIONS ?? false }), 'gpt-4o-mini')
+    if (rules.data?.length) {
+        l.star('deleting existing stream rules...')
 
-    l.star('Generating hashtags...')
+        await twitterClient.v2.updateStreamRules({
+            delete: { ids: rules.data.map(rule => rule.id) },
+        });
+    }
 
-    const hashtags = await callOpenAiApi(tweetHashtagsGeneratorTemplate({ content: tweetContent, hashtagSuggestions: HASHTAG_SUGGESTIONS }))
-    const finalTweetContent = tweetTemplate({ content: tweetContent, hashtags })
+    // Add our rules
+    l.star('Updating stream rules...')
 
-    await twitterClient.v2.tweet(finalTweetContent)
-    db.lastTweetedAtMs = Date.now()
-    await saveDB(db)
+    await twitterClient.v2.updateStreamRules({
+      add: [{ value: '(#programming AND (joke OR humour)) AND (-is:nullcast AND is:verified)' }],
+    });
+
+    l.star('Searching stream...')
+    const stream = await twitterClient.v2.searchStream({
+      'tweet.fields': ['referenced_tweets', 'author_id'],
+      expansions: ['referenced_tweets.id'],
+    });
+    // Enable auto reconnect
+    stream.autoReconnect = true;
+
+    stream.on(ETwitterStreamEvent.Data, async tweet => {
+      // Ignore RTs or self-sent tweets
+      const isARt = tweet.data.referenced_tweets?.some(tweet => tweet.type === 'retweeted') ?? false;
+      if (isARt || tweet.data.author_id === (await twitterClient.currentUserV2())?.data?.id) {
+        return;
+      }
+
+      // Reply to tweet
+      //   await twitterClient.v1.reply('Did you talk about JavaScript? love it!', tweet.data.id);
+    });
 }
 
 run().catch((err: any): void => {
-    l.error(err?.message ?? err)
+    l.error(err?.stack ?? err)
 }).then((): void => {
     l.star('Terminated.')
 })
